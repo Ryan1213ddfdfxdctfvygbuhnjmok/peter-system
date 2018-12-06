0e1ed997a5a0153d3740b34838a52adae8584412
Ryan1213ddfdfxdctfvygbuhnjmok <43233768+Ryan1213ddfdfxdctfvygbuhnjmok@users.noreply.github.com>
2018-12-06 13:40:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const devs = ['499875035964506113'];
+var prefix = "-";
+const adminprefix = "-"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -7,52 +10,104 @@ client.on('ready', () => {
 
 
 
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**مساء النور
-https://discord.gg/mTVcD8m
-**`)
-}).catch(console.error)
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
 
-})
+if (message.content.startsWith(prefix + 'setwatching')) {
+    client.user.setActivity(argresult, {type : 'watching'});
+ message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
+}
+});
+
+
+
+client.on('message', message => {
+   if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('ليس لديك صلاحية`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let BcList = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.setAuthor(`محتوى الرساله ${args}`)
+.setDescription(`برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست`)
+if (!args) return message.reply('**يجب عليك كتابة كلام لرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+msg.react('📝')
+.then(() => msg.react('✏'))
+.then(() =>msg.react('📝'))
  
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-** مساءالنور
-https://discord.gg/mTVcD8m
-**`) 
-}).catch(console.error)
+let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+ 
+let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+ 
+EmbedBc.on("collect", r => {
+message.channel.send(` تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setDescription(`${args}`)
+.setThumbnail(message.author.avatarURL)
+m.send({ embed: bc })
+msg.delete();
 })
-
-
-client.on('message', message => {
-if (message.content === 'رابط') {
-message.author.send(' سلام عليكم https://discord.gg/mTVcD8m')
-}
-});
-
-
-client.on('message', message => {
-if (message.content === '.') {
-message.author.send(' سلام عليكم https://discord.gg/mTVcD8m')
-}
-});
-
-client.on('message', message => {
-if (message.content === 'سلام عليكم') {
-message.author.send(' سلام عليكم https://discord.gg/mTVcD8m')
-}
-});
-
-client.on('guildCreate', g => {
-g.members.forEach(m => {
-if (m.voiceChannel) {
-m.send(' سلام عليكم https://discord.gg/mTVcD8m')
-}
 })
+NormalBc.on("collect", r => {
+  message.channel.send(` تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+m.send(args);
+msg.delete();
+})
+})
+})
+}
 });
+
+
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 2,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط بلخاص")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("مدة الرابط : ساعه | عدد استخدامات الرابط : 2")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
 
 
 client.login(process.env.BOT_TOKEN);
